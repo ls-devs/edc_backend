4a1a47d8f06ed7c30464ffeb2c63a39e19671d26
ls-devs <58638832+ls-devs@users.noreply.github.com>
2023-09-08 16:43:56 +0200

    allow cors

--- src/main.ts
+++ src/main.ts
@@ -21,6 +21,12 @@ import Comments from "./routes/comments";
 const app = express();
 const port = 3000;
 app.use(cors());
+app.use(function(req, res, next) {
+res.header("Access-Control-Allow-Origin", "*");
+res.header("Access-Control-Allow-Headers",
+     "Origin, X-Requested-With, Content-Type, Accept");
+next();
+});
 
 app.use("/users", Users);
 app.use("/villes", Villes);
